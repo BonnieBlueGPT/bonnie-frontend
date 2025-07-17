0d8355f5bd8fb24af7f9732c3c7a99ceee595ca0
Kai Badhan <kai@trainmygirl.com>
2025-07-17 20:50:50 +0100

    Resolved merge conflict in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,9 @@
-diff --git a/src/main.jsx b/src/main.jsx
---- a/src/main.jsx
-+++ b/src/main.jsx
-@@ -0,0 +1,9 @@
-+import React from 'react';
-+import ReactDOM from 'react-dom/client';
-+import BonnieChat from './BonnieChat_Fixed.jsx';
-+
-+ReactDOM.createRoot(document.getElementById('root')).render(
-+  <React.StrictMode>
-+    <BonnieChat />
-+  </React.StrictMode>
-+);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import BonnieChat from './BonnieChat_Fixed.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BonnieChat />
+  </React.StrictMode>
+);
